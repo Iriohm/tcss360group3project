b6280211f72513ef849373fb71c4b7f37a02936c
Iriohm <Iriohm@192.168.0.24>
2016-11-09 16:19:33 -0800

    Added more (blank) test classes.

---
+++ TCSS360_Group3_Project/src/testing/BidderTest.java
@@ -0,0 +1,8 @@
+package testing;
+
+import junit.framework.*;
+
+public class BidderTest extends TestCase {
+	
+
+}
---
+++ TCSS360_Group3_Project/src/testing/StaffTest.java
@@ -0,0 +1,8 @@
+package testing;
+
+import junit.framework.*;
+
+public class StaffTest extends TestCase {
+	
+
+}
