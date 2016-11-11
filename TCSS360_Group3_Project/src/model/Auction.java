0f718e2eb80416f269128339e181b27d720b7dd4
Justin Washburn <washbj@uw.edu>
2016-11-10 22:48:37 -0800

    Added some Java docs to calendar and uction

--- TCSS360_Group3_Project/src/model/Auction.java
+++ TCSS360_Group3_Project/src/model/Auction.java
@@ -5,26 +5,59 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The Auction Class handles adding Items to Auctions and stores the list of Items
+ * 
+ * @author Justin Washburn
+ * @version 11/10/2016
+ *
+ */
 public class Auction implements Serializable {
 
 	/**
-	 * 
+	 * Default global needed by Serializable
 	 */
 	private static final long serialVersionUID = 7183405560696200845L;
-	List<Item> myItems;
-	Date myDate;
-	String myAuctionName;
 	
+	/**
+	 * List of Items that an Auction currently has
+	 */
+	private List<Item> myItems;
+	
+	/**
+	 * The Date the Auction is being held on
+	 */
+	private Date myDate;
+	
+	/**
+	 * The name of the auction
+	 */
+	private String myAuctionName;
+	
+	
+	/**
+	 * Creates the Auction and leaves the list of items empty.
+	 * 
+	 * @param 
+	 */
 	public Auction(Date theDate, String theAuctionName) {
 		myDate = theDate;
 		myAuctionName = theAuctionName;
 		myItems = new LinkedList<Item>();
 	}
 	
+	
+	/**
+	 * 
+	 */
 	public List<Item> getItems() {
 		return myItems;
 	}
 	
+	
+	/**
+	 * 
+	 */
 	public boolean addItem(Item theItem) {
 		return false
 	}
--- TCSS360_Group3_Project/src/model/Calendar.java
+++ TCSS360_Group3_Project/src/model/Calendar.java
@@ -5,30 +5,69 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The Calendar class takes care of keeping track of Auction requests and holds all of the Aucitons.
+ * 
+ * @author Justin Washburn
+ * @version 11/10/2016
+ *
+ */
 public class Calendar implements Serializable {
 
 	/**
-	 * 
+	 * Default global needed by Serializable
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * The list of ALL of the aucitons
+	 */
 	List<Auction> myAuctions;
 	
+	/**
+	 * Creates a Calendar with an empty list of Auctions
+	 */
 	public Calendar() {
 		myAuctions = new LinkedList<Auction>();
 	}
 	
+	/**
+	 * Creates a Calendar with the given list of Auctions
+	 * 
+	 * @param theAuctions The list of auctions to be added to the Calendar
+	 */
 	public Calendar(List<Auction> theAuctions) {
 		myAuctions = theAuctions;
 	}
+	
+	/**
+	 * Returns true if the given date does not conflict with any of Auction Central's 
+	 * rules about auction scheduling
+	 * 
+	 * @param theDate The desired date for an auction request
+	 * @return True if the Auction was added
+	 */
 	public boolean validateAuctionRequest(Date theDate) {
 		return false;
 	}
 	
+	/**
+	 * Adds an Auction to the Calendar. Request must be validated before being added
+	 * 
+	 * @param theAuction The auction to be added to the calendar
+	 * @return True if the Auction was added
+	 */
 	public boolean addAuction(Auction theAuction) {
 		myAuctions.add(theAuction);
 		return false;
 	}
 	
+	/**
+	 * Gets the auctions from a period starting at the Date given and ending one month after
+	 * 
+	 * @param theDate The date to start the list of Auctions
+	 * @return The list of Auctions starting at the Date given and ending one month after
+	 */
 	public List<Auction> getAuctions(Date theDate) {
 		return null;
 	}
