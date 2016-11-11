9c39927e5dd319b6166fe814ab739f4bd28b750e
Justin Washburn <washbj@uw.edu>
2016-11-10 19:09:17 -0800

    Added method stubs to Auction and Calendar

--- TCSS360_Group3_Project/src/model/Auction.java
+++ TCSS360_Group3_Project/src/model/Auction.java
@@ -1,5 +1,33 @@
 package model;
 
-public class Auction {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Auction implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7183405560696200845L;
+	List<Item> myItems;
+	Date myDate;
+	String myAuctionName;
+	
+	public Auction(Date theDate, String theAuctionName) {
+		myDate = theDate;
+		myAuctionName = theAuctionName;
+		myItems = new LinkedList<Item>();
+	}
+	
+	public List<Item> getItems() {
+		return myItems;
+	}
+	
+	public boolean addItem(Item theItem) {
+		return false
+	}
+	
+	
 }
--- TCSS360_Group3_Project/src/model/Calendar.java
+++ TCSS360_Group3_Project/src/model/Calendar.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Calendar {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Calendar implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	List<Auction> myAuctions;
+	
+	public Calendar() {
+		myAuctions = new LinkedList<Auction>();
+	}
+	
+	public Calendar(List<Auction> theAuctions) {
+		myAuctions = theAuctions;
+	}
+	public boolean validateAuctionRequest(Date theDate) {
+		return false;
+	}
+	
+	public boolean addAuction(Auction theAuction) {
+		myAuctions.add(theAuction);
+		return false;
+	}
+	
+	public List<Auction> getAuctions(Date theDate) {
+		return null;
+	}
 }
