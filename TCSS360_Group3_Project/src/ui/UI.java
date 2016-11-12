96d0ee30b99442d75c54c7c1bc2823a4cd0a1f49
dbnowlin <blackdeath59@gmail.com>
2016-11-11 18:55:30 -0800

    test uI add form main.

--- TCSS360_Group3_Project/src/ui/UI.java
+++ TCSS360_Group3_Project/src/ui/UI.java
@@ -2,6 +2,8 @@ package ui;
 
 import java.util.Scanner;
 
+import model.User;
+
 /**
  * this start the ui.
  * 
@@ -10,7 +12,7 @@ import java.util.Scanner;
  *
  */
 public class UI {
-	private Scanner scanner = new Scanner(System.in);
+	private static Scanner myScanner = new Scanner(System.in);
 	
 	public static void begainUI() {
 		choosePreviousOrMakeUser();
@@ -25,17 +27,21 @@ public class UI {
 			System.out.println("Welcome to Auction Center\n"
 					+ "1) Login\n"
 					+ "2) crate acount\n"
-					+ "Enter your Selection from 1 to 2");
-		} while (choose != 1 || choose != 2);
+					+ "Enter your Selection from 1 to 2: ");
+			choose = myScanner.nextInt();
+		} while (choose != 1 && choose != 2);
 		if(choose == 1){
+			System.out.println("went in to login");
 			//TODO Login a user need to figure out
 		} else {
-			
+			System.out.println("went in to create User");
+			//createUser();
 		}
 	}
 	
-	private static void createUser() {
+	private static User createUser() {
 		//TODO create the the new user.
 		System.out.println("");
+		return null;
 	}
 }
