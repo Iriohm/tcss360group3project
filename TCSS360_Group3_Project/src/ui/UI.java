91cd832d3423a8165c923f52d09aba475023c1ea
dbnowlin <blackdeath59@gmail.com>
2016-11-11 17:00:56 -0800

    add a println forgot to branch.

--- TCSS360_Group3_Project/src/ui/UI.java
+++ TCSS360_Group3_Project/src/ui/UI.java
@@ -1,5 +1,9 @@
 package ui;
 
 public class UI {
-
+	public static void choosePreviousOrMakeUser() {
+		System.out.println("Welcome to Auction Center\n"
+				+ "1) Login\n"
+				+ "2) crate acount");
+	}
 }
