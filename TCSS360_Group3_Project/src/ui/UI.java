1a39316f1b535c0160e2ca6db1f7bc288a08bf13
dbnowlin <blackdeath59@gmail.com>
2016-11-11 17:45:36 -0800

    UI Add a choose to login or create user. Not done yet.

--- TCSS360_Group3_Project/src/ui/UI.java
+++ TCSS360_Group3_Project/src/ui/UI.java
@@ -1,9 +1,41 @@
 package ui;
 
+import java.util.Scanner;
+
+/**
+ * this start the ui.
+ * 
+ * @author David Nowlin
+ * @version 11/11/2016
+ *
+ */
 public class UI {
-	public static void choosePreviousOrMakeUser() {
-		System.out.println("Welcome to Auction Center\n"
-				+ "1) Login\n"
-				+ "2) crate acount");
+	private Scanner scanner = new Scanner(System.in);
+	
+	public static void begainUI() {
+		choosePreviousOrMakeUser();
+	}
+	
+	/*
+	 * this choose if it a previous user to login or crate a user
+	 */
+	private static void choosePreviousOrMakeUser() {
+		int choose = 0;
+		do {
+			System.out.println("Welcome to Auction Center\n"
+					+ "1) Login\n"
+					+ "2) crate acount\n"
+					+ "Enter your Selection from 1 to 2");
+		} while (choose != 1 || choose != 2);
+		if(choose == 1){
+			//TODO Login a user need to figure out
+		} else {
+			
+		}
+	}
+	
+	private static void createUser() {
+		//TODO create the the new user.
+		System.out.println("");
 	}
 }
