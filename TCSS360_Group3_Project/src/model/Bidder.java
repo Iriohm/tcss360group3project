dd7f598de4c092e75f37e90596e5ee6becef39bd
vk360class <class360github@gmail.com>
2016-11-12 12:20:01 -0800

    Added functionality to determine the user type.
    Key: 
    1 = Staff
    2 = NPContact
    3 = Bidder

--- TCSS360_Group3_Project/src/model/Bidder.java
+++ TCSS360_Group3_Project/src/model/Bidder.java
@@ -4,24 +4,28 @@ package model;
 * fields specifically for AuctionCentral bidders.
 *
 * @author Vlad Kaganyuk
-* @version 8 Nov 2016
+* @version 12 Nov 2016
 *
 */
 public class Bidder extends User {
 	private Calendar myCalendar;
+	
+	private static final long serialVersionUID = 7526496795622776147L;
 
 	/**
-	* This constructor sets the fields equal to the user-provided values.
+	* This constructor sets the fields equal to the user-provided values,
+	* and sets the User type to 3.
+	* @param theUsername The String that contains the User's username.
 	* @param theCalendar The Calendar object to call methods on.
     */
-	public Bidder(Calendar theCalendar) {
-		super(theCalendar);
+	public Bidder(String theUsername, Calendar theCalendar) {
+		super(theUsername, theCalendar, 3);
 	}
 
 	/**
 	* This method allows the user to place a bid on an Item object.
     */
 	public void placeBid() {
-
+		
 	}
 }
--- TCSS360_Group3_Project/src/model/NPContact.java
+++ TCSS360_Group3_Project/src/model/NPContact.java
@@ -1,5 +1,25 @@
 package model;
+/**
+* This class is used to handle/hold methods and
+* fields specifically for AuctionCentral non-profit
+* contact persons.
+*
+* @author Vlad Kaganyuk
+* @version 12 Nov 2016
+*
+**/
+public class NPContact extends User {
+	private Calendar myCalendar;
+	
+	private static final long serialVersionUID = 7526433795622776147L;
 
-public class NPContact {
-
+	/**
+	* This constructor sets the fields equal to the user-provided values,
+	* and sets the User type to 2.
+	* @param theUsername The String that contains the User's username.
+	* @param theCalendar The Calendar object to call methods on.
+    */
+	public NPContact(String theUsername, Calendar theCalendar) {
+		super(theUsername, theCalendar, 2);
+	}
 }
