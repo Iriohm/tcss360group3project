97fa05656172d32f125b1d08293190a00946e194
rhinds1004 <rhinds1004@gmail.com>
2016-11-20 17:19:18 -0800

    added method to calendar class to get auction for one day only. need to find out how to junit test new calaendar method. added method .
    added partial method to cancel bid. need to implement calling remove
    item method and any other validation.
    
    added junit tests for cancel bid and validate the bid can be cancelled
    if auction is greater than two days.

--- TCSS360_Group3_Project/src/model/Bidder.java
+++ TCSS360_Group3_Project/src/model/Bidder.java
@@ -1,31 +1,42 @@
 package model;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
 /**
-* This class is used to handle/hold methods and
-* fields specifically for AuctionCentral bidders.
-*
-* @author Vlad Kaganyuk
-* @version 12 Nov 2016
-*
-*/
+ * This class is used to handle/hold methods and fields specifically for
+ * AuctionCentral bidders.
+ *
+ * @author Vlad Kaganyuk
+ * @version 12 Nov 2016
+ *
+ */
 public class Bidder extends User {
-	// Slight alteration. Removed myUsername variable. Instead inherited from User superclass.
-	//  - M. Scott
+	// Slight alteration. Removed myUsername variable. Instead inherited from
+	// User superclass.
+	// - M. Scott
 	private static final long serialVersionUID = 7526496795622776147L;
 
 	/**
-	* This constructor sets the fields equal to the user-provided values,
-	* and sets the User type to 3.
-	* @param theUsername The String that contains the User's username.
-	* @param theCalendar The Calendar object to call methods on.
-    */
+	 * This constructor sets the fields equal to the user-provided values, and
+	 * sets the User type to 3.
+	 * 
+	 * @param theUsername
+	 *            The String that contains the User's username.
+	 * @param theCalendar
+	 *            The Calendar object to call methods on.
+	 */
 	public Bidder(String theUsername, Calendar theCalendar) {
 		super(theUsername, theCalendar, 3);
 		myUsername = theUsername;
 	}
 
 	/**
-	* This method allows the user to place a bid on an Item object.
-    */
+	 * This method allows the user to place a bid on an Item object.
+	 */
 	public boolean placeBid(Item theItemToBidOn, double thePrice) {
 		if (theItemToBidOn.getBidOf(this.myUsername) == null) {
 			theItemToBidOn.makeBid(this.myUsername, thePrice);
@@ -34,8 +45,68 @@ public class Bidder extends User {
 		return false;
 	}
 
-	public boolean cancelBid(Item myTestItem) {
+	/**
+	 * This method allows the user to cancel a bid on an Item object.
+	 * @author "Robert Hinds"
+	 */
+	public boolean cancelBid(Item theItemToCancelBidOn, Bidder theBidder) {
 		// TODO Auto-generated method stub
-		return false;
+
+		
+		if(theItemToCancelBidOn.isBeingBidOnBy(theBidder.myUsername) == false){
+			return false;
+		}
+		else if((validateCancelBidGreaterThanDayLimit(theItemToCancelBidOn, theBidder)) == false){
+			return false;
+		}
+		return true;
 	}
+	/**
+	 * This method validates that the auction is greater than cancellation deadline
+	 * @author "Robert Hinds"
+	 * @return true if the auction date of the item bid being cancelled is  greater than cancellation deadline, false otherwise. 
+	 */
+	public boolean validateCancelBidGreaterThanDayLimit(Item theItemToCancelBidOn, Bidder theBidder) {
+
+		int i = 0, j = 0;
+		int sizeOfListOfAuctionItems = 0;
+		int dayLimitToCancel = 2;
+		long differenceInMillis = 0;
+		long diffenceInDays = 0;
+		
+		GregorianCalendar theDate = (GregorianCalendar) GregorianCalendar.getInstance();
+		//TODO remove add day.
+		//theDate.add(java.util.Calendar.DATE, +15);
+		boolean foundItem = false;
+		boolean returnValue = false;
+		List<Auction> theAuctionList = theBidder.getCalendar().getTodayAuctions(theDate);
+		
+		while (i < theAuctionList.size() && foundItem == false) {
+			sizeOfListOfAuctionItems = theAuctionList.get(i).getItems().size();
+			for (j = 0; j < sizeOfListOfAuctionItems; j++) {
+				if (theAuctionList.get(i).getItems().get(j).getID() == theItemToCancelBidOn.getID()) {
+					foundItem = true;
+					break;
+				}
+			}
+			if (foundItem == true) {
+				//this check kinda iffy.
+				//theDate.add(java.util.Calendar.DATE, 4);
+				
+				//converts dates into milliseconds then converts them to days.
+				differenceInMillis = theDate.getTimeInMillis() - theAuctionList.get(i).getDate().getTimeInMillis();
+				diffenceInDays = differenceInMillis / (24 * 60 * 60 * 1000);
+				
+				if ( diffenceInDays > dayLimitToCancel) {
+					returnValue = true;
+				}
+				else{
+					returnValue = false;
+				}
+			}
+
+		}
+		return returnValue;
+	}
+
 }
