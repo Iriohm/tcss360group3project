e856bfced8261d2b5747bfe0d71fb911f9e2b2a6
rhinds1004 <rhinds1004@gmail.com>
2016-11-29 20:43:04 -0800

    init commit

---
+++ TCSS360_Group3_Project/src/testing/StaffTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package testing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.*;
+/**
+ * @author "Robert Hinds"
+ *
+ */
+public class StaffTest extends TestCase{
+
+}
