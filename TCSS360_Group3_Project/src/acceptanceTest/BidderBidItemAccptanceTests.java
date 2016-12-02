f387cd51d7f633f7cf31be07b4886af18552a6a0
dbnowlin <blackdeath59@gmail.com>
2016-12-01 21:30:01 -0800

    add the acceptance test package and the classes.

---
+++ TCSS360_Group3_Project/src/acceptanceTest/BidderBidItemAccptanceTests.java
@@ -0,0 +1,14 @@
+package acceptanceTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BidderBidItemAccptanceTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
