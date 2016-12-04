6b7a8ae81c22722879caea0409cad886197a1bdd
dbnowlin <blackdeath59@gmail.com>
2016-12-03 17:16:59 -0800

    i add in the npcontact a method that addAuction that take a auction and a calender it dose all the check to add a auctions

--- TCSS360_Group3_Project/src/acceptanceTest/NonProfitSubmitAuctionRequstAcceptanceTests.java
+++ TCSS360_Group3_Project/src/acceptanceTest/NonProfitSubmitAuctionRequstAcceptanceTests.java
@@ -2,13 +2,36 @@ package acceptanceTest;
 
 import static org.junit.Assert.*;
 
+import java.util.GregorianCalendar;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class NonProfitSubmitAuctionRequstAcceptanceTests {
+import model.Auction;
+import model.Calendar;
+import model.Item;
+import model.NPContact;
 
+public class NonProfitSubmitAuctionRequstAcceptanceTests {
+	private Calendar aCalender;
+	private NPContact myTestContact;
+	/**
+     * Initializes the myTestContact object before every test.
+     */
+    @Before
+    public void setUp() {
+    	aCalender = new Calendar();
+    	myTestContact = new NPContact("test", aCalender);
+    }
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMaximumOneFutureAuctionNoFutureAuction() {
+		GregorianCalendar fuctureAuction = (GregorianCalendar)GregorianCalendar.getInstance();
+		fuctureAuction.add(GregorianCalendar.DAY_OF_YEAR, 12);
+		Auction tesAuction = new Auction(fuctureAuction, "testAuctions");
+		Item testItem = new Item("1", "testname", "avnsdovn", "smaill", 5.00, 1, "good");
+		tesAuction.addItem(testItem);
+		assertTrue(myTestContact.addAuction(tesAuction, aCalender));
 	}
 
 }
